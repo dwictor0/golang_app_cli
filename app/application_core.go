08653e22f3a175d68888aad4997dc7496a5ff453
dwictor0 <daniel.wdev0@gmail.com>
2025-12-07 16:38:56 -0300

    FEAT: ADICIONA_COMMANDS_APLICACAO_CLI_GO

--- app/application_core.go
+++ app/application_core.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/urfave/cli"
 )
 
@@ -9,6 +13,49 @@ func Cli() *cli.App {
 	app := cli.NewApp()
 	app.Name = "go_app"
 	app.Usage = "Requests servers HTTP and see ip address"
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Get IPs address",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: func(c *cli.Context) {
+				host := c.String("host")
+				ips, erro := net.LookupNS(host)
+				if erro != nil {
+					log.Fatal(erro)
+				}
+				for _, ip := range ips {
+					fmt.Println("[!] IPs encontrados: ", ip)
+				}
+			},
+		},
+		{
 
+			Name:  "server",
+			Usage: "Get domains servers",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: func(c *cli.Context) {
+				host := c.String("host")
+				serverDomain, erro := net.LookupNS(host)
+				if erro != nil {
+					log.Fatal(erro)
+				}
+				for _, servidor := range serverDomain {
+					fmt.Println(servidor.Host)
+				}
+
+			},
+		},
+	}
 	return app
 }
