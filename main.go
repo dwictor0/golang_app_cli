6924842d1423e5730b34e06822f19f39644d7e12
dwictor0 <daniel.wdev0@gmail.com>
2025-12-07 16:42:26 -0300

    FEAT: ADICIONA_IF_INIT_PARA_TRATATIVA_DE_ERRO_MAIN_GO

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "aplicacao_terminal/app"
+import (
+	"aplicacao_terminal/app"
+	"log"
+	"os"
+)
 
 func main() {
 	aplicacao := app.Cli()
+	if erro := aplicacao.Run(os.Args); erro != nil {
+		log.Fatal(erro)
+	}
 }
